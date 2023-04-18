f6c1fd308076cd45245fb9e19826dcaa3322ca9b
Hamid-0 <habol085@uottawa.ca>
2023-04-17 20:39:57 -0400

    added documentation to htmldiff and seeprevious

--- frontend/frontend-nego/src/SoarNego/HTMLdiff.jsx
+++ frontend/frontend-nego/src/SoarNego/HTMLdiff.jsx
@@ -1,6 +1,20 @@
+/**
+ * In this file, the HTMLDiff component accepts two HTML strings, left and right, 
+ * and displays their differences using the HtmlDiffer library. 
+ * The HtmlDiffer is configured with specific options, and the compareHTML function 
+ * uses it to generate the differences between the input HTML strings. 
+ * The cleanHTML function is used to clean the generated 
+ * HTML differences for better viewing. 
+ * The component updates the displayed differences 
+ * using the useEffect hook whenever the input HTML strings change. 
+ * If both inputs are not provided, an error message is displayed.
+ * */
+
 import React, { useEffect, useRef } from "react";
+/** @library https://github.com/bem/html-differ  */ 
 import { HtmlDiffer } from "html-differ";
 
+// Create a new HtmlDiffer instance with the specified options
 const htmlDiffer = new HtmlDiffer({
   ignoreAttributes: ["line-height", "p"],
   compareAttributesAsJSON: [],
@@ -10,24 +24,29 @@ const htmlDiffer = new HtmlDiffer({
   ignoreDuplicateAttributes: false,
 });
 
+// Function to compare the left and right HTML content and return the differences
 const compareHTML = (left, right) => {
   const diff = htmlDiffer.diffHtml(left, right);
   return diff;
 };
 
+// Function to clean the HTML using DOMParser for better viewing
 const cleanHTML = (html) => {
   const parser = new DOMParser();
   const doc = parser.parseFromString(html, "text/html");
   return doc.body.innerHTML;
 };
 
+// HTMLDiff component accepts left and right HTML content and displays their differences
 const HTMLDiff = ({ left, right }) => {
   const containerRef = useRef(null);
 
+  // Update the displayed differences whenever the left or right content changes
   useEffect(() => {
     if (containerRef.current && left && right) {
       const differences = compareHTML(left, right);
 
+      // Map the differences to appropriate HTML elements (ins and del) and join the result
       const result = differences
         .map((part) => {
           if (part.added) {
@@ -40,15 +59,18 @@ const HTMLDiff = ({ left, right }) => {
         })
         .join("");
 
+      // Clean the result using the cleanHTML function
       const cleanedResult = cleanHTML(result);
       containerRef.current.innerHTML = cleanedResult;
     }
   }, [left, right]);
 
+  // Return an error message if both inputs are not provided
   if (!left || !right) {
     return <div>Both inputs must be provided.</div>;
   }
 
+  // Render the container div and set its ref to containerRef
   return <div ref={containerRef}></div>;
 };
 
