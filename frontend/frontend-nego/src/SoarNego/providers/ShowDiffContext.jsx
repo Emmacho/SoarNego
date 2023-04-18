777d6ed5a5df0d928d9352a575d960d05d22dd18
Hamid-0 <habol085@uottawa.ca>
2023-04-17 21:01:04 -0400

    added documenation for editor and showDiffContext

--- frontend/frontend-nego/src/SoarNego/providers/ShowDiffContext.jsx
+++ frontend/frontend-nego/src/SoarNego/providers/ShowDiffContext.jsx
@@ -1,13 +1,19 @@
-// ShowDiffContext.js
-/**This code creates a context using React's createContext function and exports a provider component that wraps its child components with the context's value.  */
+// This code creates a context using React's createContext function and exports
+// a provider component that wraps its child components with the context's value.
 import { createContext, useState } from 'react';
 
+// Create a new context for handling the visibility of the differences view
 const ShowDiffContext = createContext();
 
+// Define the provider component for the ShowDiffContext
 export const ShowDiffProvider = ({ children }) => {
-  const [showDiff, setShowDiff] = useState(false); //The context holds the state of the showDiff variable, which is initially set to false using the useState hook. 
+  // The context holds the state of the showDiff variable, which is initially
+  // set to false using the useState hook.
+  const [showDiff, setShowDiff] = useState(false);
 
-//The provider component returns the context's provider with the value of an object that contains the current state of showDiff and the function to update it, setShowDiff.
+  // The provider component returns the context's provider with the value of an
+  // object that contains the current state of showDiff and the function to
+  // update it, setShowDiff.
   return (
     <ShowDiffContext.Provider value={{ showDiff, setShowDiff }}>
       {children}
@@ -15,4 +21,5 @@ export const ShowDiffProvider = ({ children }) => {
   );
 };
 
+// Export the ShowDiffContext
 export default ShowDiffContext;
